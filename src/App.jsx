38ef346198723aaea76eb4d25b690119e93aa88c
Manu Artero Anguita <manutero.developer@gmail.com>
2024-03-15 11:46:54 +0100

    minor suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,71 @@
-// App.jsx
-import React, { useState } from 'react';
-import TodoItem from './TodoItem';
-import LoginComponent from './Login'; // Importa el nuevo componente de inicio de sesión
-import './App.css';
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+import LoginComponent from "./Login";
+import "./App.css";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Nuevo estado para controlar el inicio de sesión
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
-      setNewTodo('');
-    }
-  };
-  const editTodo = (id, newText) => {
-    setTodos(todos.map(todo => (todo.id === id ? { ...todo, text: newText } : todo)));
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
-  const toggleComplete = (id) => {
-    setTodos(todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
-  };
-
-  const onLogin = (token) => {
-    // Esta función se llama desde el componente de inicio de sesión cuando se ha iniciado sesión correctamente
-    // Puedes usar el token para realizar acciones adicionales si es necesario
+  const onLogin = () => {
     setIsLoggedIn(true);
   };
 
   return (
     <div className="container">
-      {isLoggedIn ? (
-        // Mostrar la lista de TODOs si el usuario ha iniciado sesión
-        <>
-          <h1>Lista TODO</h1>
-          <input
-            type="text"
-            placeholder="Añadir Tarea"
-            value={newTodo}
-            onChange={(e) => setNewTodo(e.target.value)}
-          />
-          <button onClick={addTodo}>Añadir TODO</button>
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onEdit={editTodo}
-              onDelete={deleteTodo}
-              onToggleComplete={toggleComplete}
-            />
-          ))}
-        </>
-      ) : (
-        // Mostrar el componente de inicio de sesión si el usuario no ha iniciado sesión
-        <LoginComponent onLogin={onLogin} />
-      )}
+      {isLoggedIn ? <TodoList /> : <LoginComponent onLogin={onLogin} />}
     </div>
   );
 };
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+      setNewTodo("");
+    }
+  };
+  const editTodo = (id, newText) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <>
+      <h1>Lista TODO</h1>
+      <input
+        type="text"
+        placeholder="Añadir Tarea"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Añadir TODO</button>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onEdit={editTodo}
+          onDelete={deleteTodo}
+          onToggleComplete={toggleComplete}
+        />
+      ))}
+    </>
+  );
+}
 
 export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,71 +1,79 @@
-import React, { useState } from 'react';
-import './Login.css';
+import { useState } from "react";
+import "./Login.css";
+import PropTypes from "prop-types";
 
 const LoginComponent = ({ onLogin }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const handleLogin = async () => {
-        setError('');
+  const handleLogin = async () => {
+    setError("");
 
-        try {
-            const response = await fetch('https://reqres.in/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-                // La API de Reqres.in tiene ciertos usuarios predefinidos para la autenticación.
-                //Usuario predefinido:eve.holt@reqres.in
-                //contraseña predefinida:cityslicka
-            });
+    if (!username && !password) {
+      setError("Por favor, ingresa un nombre de usuario y una contraseña");
+      return;
+    } else if (!username) {
+      setError("Por favor, ingresa un nombre de usuario");
+      return;
+    } else if (!password) {
+      setError("Por favor, ingresa una contraseña");
+      return;
+    }
 
-            if (response.ok) {
-                onLogin();
-            } else {
-                if (!username && !password) {
-                    setError('Por favor, ingresa un nombre de usuario y una contraseña');
-                } else if (!username) {
-                    setError('Por favor, ingresa un nombre de usuario');
-                } else if (!password) {
-                    setError('Por favor, ingresa una contraseña');
-                } else{
-                    setError("Usuario no predefinido para la API de Reqres.in");
-                }
-            }
-        } catch (error) {
-            setError('Error de red');
-        }
-    };
+    try {
+      const response = await fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+        // La API de Reqres.in tiene ciertos usuarios predefinidos para la autenticación.
+        //Usuario predefinido:eve.holt@reqres.in
+        //contraseña predefinida:cityslicka
+      });
 
-    return (
-        <div>
-            <h2 className="login-header">Inicio de Sesión</h2>
-            <div className={`input-container ${!username && error ? 'error' : ''}`}>
-                <label className="input-label">Usuario:</label>
-                <input
-                    type="text"
-                    className={`input-field ${!username && error ? 'error' : ''}`}
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div className={`input-container ${!password && error ? 'error' : ''}`}>
-                <label className="input-label">Contraseña:</label>
-                <input
-                    type="password"
-                    className={`password-field ${!password && error ? 'error' : ''}`}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            {error && <div className="error-message">{error}</div>}
-            <button className="login-button" onClick={handleLogin}>
-                Iniciar Sesión
-            </button>
-        </div>
-    );
+      if (response.ok) {
+        onLogin();
+      } else {
+        setError("Usuario no predefinido para la API de Reqres.in");
+      }
+    } catch (error) {
+      setError("Error de red");
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="login-header">Inicio de Sesión</h2>
+      <div className={`input-container ${!username && error ? "error" : ""}`}>
+        <label className="input-label">Usuario:</label>
+        <input
+          type="text"
+          className={`input-field ${!username && error ? "error" : ""}`}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className={`input-container ${!password && error ? "error" : ""}`}>
+        <label className="input-label">Contraseña:</label>
+        <input
+          type="password"
+          className={`password-field ${!password && error ? "error" : ""}`}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {error && <div className="error-message">{error}</div>}
+      <button className="login-button" onClick={handleLogin}>
+        Iniciar Sesión
+      </button>
+    </div>
+  );
+};
+
+LoginComponent.propTypes = {
+  onLogin: PropTypes.func.isRequired,
 };
 
 export default LoginComponent;
